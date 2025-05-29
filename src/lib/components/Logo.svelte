c0ecd41f3a2ac3f7639c46cb5a4b513932ddea20
Uncle Common <krishnerkar@gmail.com>
2025-05-29 19:25:14 +0000

    Generated website updates for version: 755

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -3,7 +3,8 @@
 	const props: HTMLAttributes<SVGSVGElement> = $props();
 </script>
 
-<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="" {...props}>
-	<rect x="4" y="4" width="11" height="11" rx="2" fill="currentColor" />
-	<rect x="9" y="9.5" width="10" height="10" rx="2" fill="currentColor" fill-opacity="0.24" />
+<svg viewBox="0 0 97 97" fill="none" xmlns="http://www.w3.org/2000/svg" class="" {...props}>
+<path opacity="0.3" d="M96.9121 67.6667V29C96.9121 12.9837 84.0179 0 68.1121 0H29.7121L0.912109 29H53.7121C61.665 29 68.1121 35.4919 68.1121 43.5V96.6667L96.9121 67.6667Z" fill="currentColor"/>
+<path d="M0.912113 29L0.912109 67.6667C0.912108 83.6829 13.8063 96.6667 29.7121 96.6667H68.1121L96.9121 67.6667H48.9121C38.3082 67.6667 29.7121 59.0109 29.7121 48.3333V0L0.912113 29Z" fill="currentColor"/>
 </svg>
+
